d13e33870b9b5526a5b6980a60892b183079b0cf
Arilsaputra854 <Arilsaputra854@gmail.com>
2024-10-29 13:19:08 +0700

    add splash screen & onboarding(1-3)

---
+++ lib/ui/onboarding/onboarding_screen.dart
@@ -0,0 +1,67 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:jahit_baju/ui/login/login_screen.dart';
+import 'package:onboarding/onboarding.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  List<ContentConfig> listContentConfig = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    listContentConfig.add(
+      const ContentConfig(
+        backgroundImage: "assets/onboarding/onboarding_1.png",
+        backgroundFilterColor: Colors.transparent,
+
+        backgroundFilterOpacity: 0.0
+      ),
+    );
+    listContentConfig.add(
+      const ContentConfig(
+        backgroundImage: "assets/onboarding/onboarding_2.png",
+        backgroundFilterColor: Colors.transparent,
+        
+        backgroundFilterOpacity: 0.0
+      ),
+    );
+    listContentConfig.add(
+      const ContentConfig(
+        backgroundImage: "assets/onboarding/onboarding_3.png",
+        backgroundFilterColor: Colors.transparent,
+        
+        backgroundFilterOpacity: 0.0
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: IntroSlider(
+      key: UniqueKey(),
+      listContentConfig: listContentConfig,
+      onDonePress: () => goToLoginPage(),
+      onSkipPress: () =>  goToLoginPage(),
+    ),
+    );
+  }
+
+
+  goToLoginPage() {
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+
+}
---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:jahit_baju/ui/onboarding/onboarding_screen.dart';
+
+import 'login/login_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var deviceWidth = MediaQuery.of(context).size.width;
+
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => OnboardingScreen()));
+    });
+
+    return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color(0xFF57AFF9),
+              Color(0xFF8BE0E5),
+              Color(0xFFDFCFAF),
+              Color(0xFFFDCA8A),
+              Color(0xFFFEAEA9),
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft)
+          ),
+      child: Center(
+        child: Container(
+            width: 250,
+            height: 250,
+            child: Image.asset("assets/logo/jahit_baju_logo.png")),
+      ),
+    ));
+  }
+}
