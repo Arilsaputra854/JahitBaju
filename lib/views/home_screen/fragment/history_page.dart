39bfc4d12697b584a59567b5651ddf69588ea810
Arilsaputra854 <Arilsaputra854@gmail.com>
2024-11-06 17:06:53 +0700

    add home, history, profile fragment

---
+++ lib/views/home_screen/fragment/history_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({super.key});
+
+  @override
+  State<HistoryPage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+          child: Container(
+          ),
+        );
+  }
+}
--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:jahit_baju/views/home_screen/fragment/history_page.dart';
+import 'package:jahit_baju/views/home_screen/fragment/home_page.dart';
+import 'package:jahit_baju/views/home_screen/fragment/profile_page.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +11,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int _indexPage = 0;
+
+  final List<Widget> page =[
+    HomePage(),
+    HistoryPage(),
+    ProfilePage()
+  ];
+
+  onItemTapped(int index){
+    setState(() {
+      _indexPage = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var deviceWidth = MediaQuery.of(context).size.width;
-    final List<String> items = List<String>.generate(20, (i) => "Item $i");
 
     return Scaffold(
         appBar: AppBar(
@@ -25,176 +41,17 @@ class _HomeScreenState extends State<HomeScreen> {
             IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart))
           ],
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.black,
-                  height: 300,
-                  width: deviceWidth,
-                  child: Image.asset(
-                    alignment: Alignment(1, -0.3),
-                    "assets/background/bg.png",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Container(
-                    margin: EdgeInsets.only(top: 10, bottom: 10),
-                    height: 100,
-                    child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: items.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            width: 80,
-                            height: 80,
-                            margin: EdgeInsets.symmetric(horizontal: 10),
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0xFFFFAAAA)),
-                            child: Center(
-                              child: Text(
-                                items[index],
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 15),
-                              ),
-                            ),
-                          );
-                        })),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Siap Pakai",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 30),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                    margin: EdgeInsets.all(10),
-                    height: 200,
-                    child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: items.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            width: 150,
-                            height: 200,
-                            margin: EdgeInsets.symmetric(horizontal: 5),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                border: Border.all(width: 1)),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: Container(
-                                    width: double.infinity,
-                                    child: Image.asset(
-                                      "assets/background/bg.png",
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                    margin: EdgeInsets.all(5),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          "Blazer",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 20),
-                                        ),
-                                        Text(
-                                          "IDR 500.000",
-                                          style: TextStyle(fontSize: 15),
-                                        ),
-                                      ],
-                                    ))
-                              ],
-                            ),
-                          );
-                        })),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Custom Produk",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 30),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                    margin: EdgeInsets.all(10),
-                    height: 200,
-                    child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: items.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            width: 150,
-                            height: 200,
-                            margin: EdgeInsets.symmetric(horizontal: 5),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                border: Border.all(width: 1)),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: Container(
-                                    width: double.infinity,
-                                    child: Image.asset(
-                                      "assets/background/bg.png",
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                    margin: EdgeInsets.all(5),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          "Blazer",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 20),
-                                        ),
-                                        Text(
-                                          "IDR 500.000",
-                                          style: TextStyle(fontSize: 15),
-                                        ),
-                                      ],
-                                    ))
-                              ],
-                            ),
-                          );
-                        })),
-                SizedBox(
-                  height: 40,
-                ),
-              ],
-            ),
-          ),
-        ));
+        body: page[_indexPage],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: onItemTapped,
+          currentIndex: _indexPage,
+          items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+        ]));
   }
 }
+
+
+
